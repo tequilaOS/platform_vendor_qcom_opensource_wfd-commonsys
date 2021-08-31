96053a8cf9bf5db370291fa97efb4e52eb717c04
Shivam Agrawal <shivagra@codeaurora.org>
2021-08-31 13:07:56 +0530

    opensource-wfd: Removing libaac from system side
    The lib is not used by system partition and
    hence being removed.
    
    Change-Id: I592ff69f93715bfa1e1f64fb6cda8f2ea50b6ce6

--- Android.mk
+++ Android.mk
@@ -7,8 +7,15 @@ WFD_DISABLE_PLATFORM_LIST := bengal
 endif
 endif
 
+# ------------------------------------------------------------------------------
+# Guard compilation of vendor projects from qssi/qssi_32 as these modules
+# would be scanned during initial stages of qssi/qssi_32 compilation.
+# non-src shippable deps of such such modules would raise compilation issues
+# in qssi/qssi_32 customer variant compilation.
+ifneq ($(call is-product-in-list, qssi qssi_32),true)
 ifneq ($(call is-board-platform-in-list,$(WFD_DISABLE_PLATFORM_LIST)),true)
 ifneq ($(TARGET_HAS_LOW_RAM), true)
 include $(call all-makefiles-under, $(LOCAL_PATH))
+endif
 endif
 endif
--- libaac/Android.mk
+++ libaac/Android.mk
@@ -1,37 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-
-#--------------------------------------------------------------------------------
-# BUILD FOR SYSTEM PARTITION
-#Guard compilation of system modules from vendor-target as these modules would be
-#scanned during vendor compilation. With introduction of groups feature, this would
-#raise compilation issues as certain commonsys deliverables would be missing
-ifeq ($(call is-product-in-list, qssi qssi_32),true)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := aacEncode.cpp
-LOCAL_SRC_FILES += aacDecode.cpp
-
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_C_INCLUDES += $(TOP)/external/aac/libAACenc/include
-LOCAL_C_INCLUDES += $(TOP)/external/aac/libAACdec/include
-LOCAL_C_INCLUDES += $(TOP)/external/aac/libSYS/include
-
-LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_SHARED_LIBRARIES += libminijail
-LOCAL_SHARED_LIBRARIES += libavservices_minijail
-LOCAL_SHARED_LIBRARIES += vendor.display.config@2.0
-LOCAL_SHARED_LIBRARIES += libdisplayconfig.qti
-LOCAL_STATIC_LIBRARIES := libFraunhoferAAC
-
-LOCAL_32_BIT_ONLY := true
-LOCAL_MODULE := libwfdaac
-LOCAL_CFLAGS += -O0
-LOCAL_SYSTEM_EXT_MODULE := true
-LOCAL_SANITIZE := integer_overflow
-include $(BUILD_SHARED_LIBRARY)
-endif
-
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := aacEncode.cpp
